46acbb4d5afecd61f2ceac313572b4a330177046
Олег <olegcelysev778@gmail.com>
2023-06-29 11:35:16 +0300

    Домашка 11

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,43 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        checkLeapYear(2001);
+        checkClientOS(1, 2015);
+        deliveryDays(95);
+    }
+    public static void checkLeapYear (int year) {
+        System.out.println("Задача 1");
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("Год " + year + " високосный");
+        } else {
+            System.out.println("Год " + year + " невисокосный");
+        }
+    }
+    public static void checkClientOS (int clientOS, int clientDeviceYear) {
+        System.out.println("Задача 2");
+        int iOs = 1;
+        int android = 0;
+        if (clientOS == iOs && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == android && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите приложение по ссылке");
+        }
+    }
+    public static void deliveryDays (int deliveryDistance) {
+        System.out.println("Задача 3");
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: " + 1);
+        } else if (deliveryDistance >= 20 && deliveryDistance <= 60) {
+            System.out.println("Потребуется дней: " + 2);
+        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: " + 3);
+        } else {
+            System.out.println("Доставки нет");
         }
     }
 }
+
+
+
+
